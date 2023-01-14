a939a82b140f1f5123f84336367abe8609222aa3
SkyBlaise <https://github.com/SkyBlaise99>
2023-01-14 13:20:43 +0800

    update sql

--- db.sql
+++ db.sql
@@ -1,11 +1,11 @@
 CREATE TABLE users (
     id      SERIAL  PRIMARY KEY,
     name    TEXT    NOT NULL,
-    score   INT     NOT NULL,
+    score   INT     NOT NULL
 );
 
 INSERT INTO users (name, score) VALUES ('Wen Hao', 10);
 INSERT INTO users (name, score) VALUES ('Shi Chen', 11);
 INSERT INTO users (name, score) VALUES ('Angler', 9);
 
-SELECT * FROM users;
+SELECT * FROM users ORDER BY score;
